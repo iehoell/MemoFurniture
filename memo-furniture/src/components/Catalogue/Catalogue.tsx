94fdf8c453337af6fbb2331b96ba28d2bf01a54a
iehoell <sashaotmachov@gmail.com>
2024-03-17 21:12:01 +0300

    Добавил маршрутизацию с помощью react dom
    Добавил маршрутизацию с помощью react dom

---
+++ memo-furniture/src/components/Catalogue/Catalogue.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import headerStyles from "./headerStyles.module.scss";
+
+function Catalogue(){
+    return(
+        <div>
+            Catalogue
+        </div>
+    );
+}
+export default Catalogue;
---
+++ memo-furniture/src/components/ProductPage/ProductPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import headerStyles from "./headerStyles.module.scss";
+
+function ProductPage(){
+    return(
+        <div>
+            PRODUCT PAGE
+        </div>
+    );
+}
+export default ProductPage;
