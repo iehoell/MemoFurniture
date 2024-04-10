ccc50ae259152ec44677c04890726e90c146b5b6
iehoell <sashaotmachov@gmail.com>
2024-04-10 08:58:01 +0300

    Добавил компонент слайдера
    Добавил компонент слайдера

--- memo-furniture/src/components/Slider/Slider.tsx
+++ memo-furniture/src/components/Slider/Slider.tsx
@@ -1,23 +1,45 @@
 import React, {Component} from 'react';
-import sliderStyles from './sliderStyles.module.scss';
-import {Carousel, CarouselItem} from "react-bootstrap";
+import './sliderStyles.css';
+import Flickity from 'react-flickity-component';
 import chair1 from '../../assets/chair1.jpg';
 import chair2 from '../../assets/chair2.jpg';
 import chair3 from '../../assets/chair3.jpg';
+import _default from "chart.js/dist/plugins/plugin.tooltip";
 
+const flickityOptions = {
+    initialIndex: 2
+}
+
+const items = {
+    id: [1, 2, 3],
+    title: ['chair1', 'chair2', 'chair3'],
+    image: [chair1, chair2, chair3]
+}
 function Slider(){
     return(
-        <Carousel>
-            <CarouselItem>
-                <img src={chair1} className={sliderStyles.sliderItem} alt={'error1'}/>
-            </CarouselItem>
-            {/*<CarouselItem>*/}
-            {/*    <img src={chair2} className={sliderStyles.sliderItem}/>*/}
-            {/*</CarouselItem>*/}
-            {/*<CarouselItem>*/}
-            {/*    <img src={chair3} className={sliderStyles.sliderItem}/>*/}
-            {/*</CarouselItem>*/}
-        </Carousel>
+        <Flickity
+            className='Slider'
+            elementType='div'
+            disableImagesLoaded={false}
+            options={flickityOptions}
+            reloadOnUpdate
+            static
+            >
+            {items['id'].map((index) => {
+                return(
+                    <div key={index} className='Plate'>
+                        <>
+                        <div style={{
+                            backgroundImage: `url(${items['image'][index-1]})`,
+                            width: 400,
+                            height: 500,
+                            backgroundSize: "cover",
+                        }}></div>
+                        </>
+                    </div>
+                )
+            })}
+        </Flickity>
     );
 }
 
